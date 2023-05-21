b007d16f58f9ee012dd194004ff38fce1fd256ce
ala1235 <ala.benhassine@esprit.tn>
2023-05-21 23:16:16 +0100

    dockerfile

---
+++ dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:11
+ADD /target/ExamThourayaS2-0.0.1-SNAPSHOT.jar ExamThourayaS2-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java", "-jar", "tpThourayaProject.jar"]
--- src/main/java/tn/esprit/spring/entities/Train.java
+++ src/main/java/tn/esprit/spring/entities/Train.java
@@ -19,6 +19,8 @@ public class Train implements Serializable {
 
     long codeTrain;
 
+    //alaBenHASSINE
+
     public long getCodeTrain() {
         return codeTrain;
     }
