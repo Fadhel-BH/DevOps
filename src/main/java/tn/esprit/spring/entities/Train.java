dd49c19f40ae691292e339583cc53107a4bf6436
yassineBinDriftin <yassine.abdelmoula@esprit.tn>
2023-05-22 13:39:53 +0200

    first commit

--- src/main/java/tn/esprit/spring/entities/Train.java
+++ src/main/java/tn/esprit/spring/entities/Train.java
@@ -8,7 +8,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//yassineABD for first commit
 @Entity
 public class Train implements Serializable {
 
